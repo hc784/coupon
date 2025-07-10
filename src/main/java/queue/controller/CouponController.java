7143c6d9908ea218e026a55a4da175bc064f8fb7
hc784 <kkjj1211@naver.com>
2025-07-10 14:03:31 +0900

    .

--- src/main/java/queue/controller/CouponController.java
+++ src/main/java/queue/controller/CouponController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
-import queue.service.CouponService;
 import queue.service.QueueService;
 
 @RestController
